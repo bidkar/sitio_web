d52b12e19bb13dd5981fe73b95c6451566deaeaa
bidkar <bidkar@gmail.com>
2017-01-26 12:03:51 -0700

    Conexion + Usuario
    se creo la clase conexion y se inicio el desarrollo de la clase usuario

--- conexion.php
+++ conexion.php
@@ -1,4 +1,19 @@
 <?php
 class Conexion extends mysqli {
     
+    public function __construct() {
+        $host = '127.0.0.1';
+        $username = 'root';
+        $passwd = 'toor';
+        $dbname = 'udom';
+        $port = '3306';
+        parent::__construct($host,$username,$passwd,$dbname,$port);
+    }
+
+    public function __construct1($host,$username,$passwd,$dbname,$port) {
+        parent::__construct($host,$username,$passwd,$dbname,$port);
+    }
 }
+
+// $cnn = new Conexion();
+// var_dump($cnn);
--- usuarios.php
+++ usuarios.php
@@ -1,4 +1,36 @@
 <?php
-class Usuarios extends Conexion {
-    
+require 'conexion.php';
+class Usuario {
+    private $datos = [
+        'id' => '',
+        'username' => '',
+        'nombre' => '',
+        'apellidos' => '',
+        'email' => ''
+    ];
+
+    public static function login($username, $passwd) {
+        $cnn = new Conexion();
+        $sql = sprintf("select * from usuarios where username='%s' and passwd='%s'", $username, md5($passwd));
+        $rst = $cnn->query($sql);
+        if (!$rst) {
+            return 'error en la consulta';
+        } else {
+            if ($rst->num_rows == 1) {
+                // agregar codigo para recoger valores de usuario
+            } else {
+                return 'no hay registros';
+            }
+        }
+    }
+
+    public function __get($campo) {
+        return $this->datos[$campo];
+    }
+
+    public function __set($campo, $valor) {
+        $this->datos[$campo] = $valor;
+    }
 }
+
+$usuario = Usuario::login('bidkar','123');
