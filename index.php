094f162eed2edc9e000ab32df218c6e5ff3e7056
bidkar <bidkar@gmail.com>
2017-02-16 11:59:00 -0700

    materialize
    jquery, fonts, materialize css

--- index.php
+++ index.php
@@ -13,52 +13,55 @@ if (isset($_SESSION['usuario'])) {
   <meta name="description" content="Sitio web de la udeo de sistemas computacionales">
   <meta name="author" content="Bidkar Aragon Cardenas">
   <title>Titulo</title>
-  <!-- CSS External -->
+  <link rel="stylesheet" href="css/materialize.css">
   <link rel="stylesheet" href="css/styles.css">
-  <!-- CSS Document -->
-  <style>
-  	/* Reglas y propiedades CSS */
-  </style>
 </head>
 <body>
-	<!-- CSS Inline -->
-	<!--<header style="background-color: brown;">-->
-  	<!--header>h1{Universidad de Occidente}+h3{Unidad Guasave}-->
-	<header id="encabezado">
+	<header id="encabezado" class="section">
 		<h1>Universidad de Occidente</h1>
 		<h3>Unidad Guasave</h3>
 	</header>
-	<main id="principal">
-		<form id="login-form" method="post" action="login.php">
-			<?php if (isset($_GET['error'])) { ?>
-			<div class="error">
-				Usuario o contraseña incorrecta
+	<main id="principal" class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col s12 m4 offset-m4">
+					<div class="card-panel">
+						<form id="login-form" method="post" action="login.php">
+							<?php if (isset($_GET['error'])) { ?>
+							<div class="error">
+								Usuario o contraseña incorrecta
+							</div>
+							<?php } ?>
+							<div class="input-field">
+								<input type="text" name="txtUsuario" id="txtUsuario">
+								<label for="txtUsuario" class="negrita">Usuario:</label>
+							</div>
+							<div class="input-field">
+								<input type="password" name="txtPasswd" id="txtPasswd">
+								<label for="txtPasswd" class="negrita">Contraseña:</label>
+							</div>
+							<div>
+								<input type="checkbox" name="remember" id="remember" class="filled-in">
+								<label for="remember">Mantener la sesion iniciada</label>
+							</div>
+							<div>
+								<button type="submit" class="btn btn-block waves-effect waves-light">Iniciar sesión</button>
+							</div>
+							<a href="lost.html" class="text-center block">¿Olvidaste tu contraseña?</a>
+						</form>
+					</div>
+				</div>
 			</div>
-			<?php } ?>
-			<div class="form-control">
-				<label for="txtUsuario" class="negrita">Usuario:</label>
-				<input type="text" name="txtUsuario" id="txtUsuario">
-			</div>
-			<div class="form-control">
-				<label for="txtPasswd" class="negrita">Contraseña:</label>
-				<input type="password" name="txtPasswd" id="txtPasswd">
-			</div>
-			<div class="form-control">
-				<label for="remember" class="negrita">
-					<input type="checkbox" name="remember" id="remember">
-					Mantener la sesion iniciada
-				</label>
-			</div>
-			<div class="form-control">
-				<button type="submit">Iniciar sesión</button>
-			</div>
-			<a href="lost.html" class="text-center block">¿Olvidaste tu contraseña?</a>
-		</form>
+		</div>
 	</main>
-	<footer id="pie">
-		<hr>
-		<p class="negrita">Todos los derechos reservados &copy; 2017</p>
+	<div class="divider"></div>
+	<footer id="pie" class="page-footer">
+		<div class="container">
+			<p class="negrita center-align">Todos los derechos reservados &copy; 2017</p>
+		</div>
 	</footer>
+	<script src="js/jquery-3.1.1.js"></script>
+	<script src="js/materialize.js"></script>
 	<script src="js/app.js"></script>
 </body>
 </html>
